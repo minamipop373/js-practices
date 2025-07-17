d776c5da048ddb7179b6a7a8be8188db7f41f5d0
minami <katominami0812@gmail.com>
2025-07-17 11:31:56 +0900

    変数を宣言して初期値を消しました

--- 01.fizzbuzz/fizzbuzz.js
+++ 01.fizzbuzz/fizzbuzz.js
@@ -1,12 +1,14 @@
 #!/usr/bin/env node
 for (let number = 1; number <= 20; number++) {
+  let output;
   if (number % 3 === 0 && number % 5 === 0) {
-    console.log("FizzBuzz");
+    output = "FizzBuzz";
   } else if (number % 3 === 0) {
-    console.log("Fizz");
+    output = "Fizz";
   } else if (number % 5 === 0) {
-    console.log("Buzz");
+    output = "Buzz";
   } else {
-    console.log(String(number));
+    output = String(number);
   }
+  console.log(output);
 }
