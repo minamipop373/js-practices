3afbed8b1681ddf404fb1abc525005f475f4fcdc
minami <katominami0812@gmail.com>
2025-07-28 23:50:15 +0900

    土曜日と月末の余白を削除

--- 02.calendar/cal.js
+++ 02.calendar/cal.js
@@ -33,8 +33,13 @@ const lastDate = new Date(year, month + 1, 0);
 let output = "   ".repeat(firstDate.getDay());
 
 for (let day = 1; day <= lastDate.getDate(); day++) {
-  const lineBreak = (firstDate.getDay() + day) % 7 === 0 ? "\n" : "";
-  output = `${output}${String(day).padStart(2, " ")} ${lineBreak}`;
-}
+  const paddedDay = String(day).padStart(2, " ");
+  const isSaturday = (firstDate.getDay() + day - 1) % 7 === 6;
+  const isLastDay = day === lastDate.getDate();
 
+  output += paddedDay;
+  if (!isLastDay) {
+    output += isSaturday ? "\n" : " ";
+  }
+}
 console.log(output);
